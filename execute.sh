16413db94426e40bd44aac3e69e0a22461ed6c28
Berry van Mondfrans <personal@berryvm.com>
2018-03-08 09:03:08 +0100

    Added git autocompletion

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in ~/terminal/scripts/*.sh; do
+for file in ~/terminal/scripts/*; do
     [ -f "$file" ] && [ -x "$file" ] && . $file || echo "Could not execute $file"
 done
 echo "All terminal scripts loaded!"
